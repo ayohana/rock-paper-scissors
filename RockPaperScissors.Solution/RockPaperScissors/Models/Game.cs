fd0f122bf2dbc0d0069dfc8eca0c2199b90104fd
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-04 15:46:43 -0800

    Add a try/catch block to Program.cs

--- RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Models/Game.cs
@@ -47,6 +47,7 @@ namespace RockPaperScissors.Models
       }
     }
 
+    
     // public string CheckWinner()
     // {
     //   if (Player1Sign == "rock")
--- RockPaperScissors.Solution/RockPaperScissors/Program.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Program.cs
@@ -8,27 +8,36 @@ namespace RockPaperScissors
   {
     public static void Main()
     {
+      try
+      {
+        Console.WriteLine("Enter Player 1's name:");
+        string name1 = Console.ReadLine();
+        Console.WriteLine("Enter Player 2's name:");
+        string name2 = Console.ReadLine();
+        Game game = new Game(name1, name2);
+        Console.WriteLine(game.Player1.Name);
+        Console.WriteLine(game.Player2.Name);
 
-      Console.WriteLine("Enter Player 1's name:");
-      string name1 = Console.ReadLine();
-      Console.WriteLine("Enter Player 2's name:");
-      string name2 = Console.ReadLine();
-      Game game = new Game(name1, name2);
-      Console.WriteLine(game.Player1.Name);
-      Console.WriteLine(game.Player2.Name);
+        Console.WriteLine("Enter player 1's sign:");
+        string input1 = Console.ReadLine().ToLower();
+        Console.WriteLine("Enter player 2's sign:");
+        string input2 = Console.ReadLine().ToLower();
+        game.AssignSigns(input1, input2);
 
-      Console.WriteLine("Enter player 1's sign:");
-      string input1 = Console.ReadLine().ToLower();
-      Console.WriteLine("Enter player 2's sign:");
-      string input2 = Console.ReadLine().ToLower();
-      game.AssignSigns(input1, input2);
+        Console.WriteLine(game.Player1.Sign);
+        Console.WriteLine(game.Player2.Sign);
 
-      Console.WriteLine(game.Player1.Sign);
-      Console.WriteLine(game.Player2.Sign);
-
-      
-      int result = game.CheckResult();
-      AnnounceWinner(result);
+        int result = game.CheckResult();
+        AnnounceWinner(result);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("HelpLink = {0}", ex.HelpLink);
+        Console.WriteLine("Message = {0}", ex.Message);
+        Console.WriteLine("Source = {0}", ex.Source);
+        Console.WriteLine("StackTrace = {0}", ex.StackTrace);
+        Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+      }
     }
 
     public static void AnnounceWinner(int result)
